6ae8de84aa1e8957f732e0697b53bc2628386077
Joomonteiro <montjpedro@gmail.com>
2022-10-12 19:05:52 -0300

    feat: EW-3 configurações do banco e integração ao typeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
+import { UsersModule } from './users-test/users.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/user.entity';
+import { UsersController } from './users/users.controller';
 
 @Module({
-  imports: [UsersModule],
-  controllers: [AppController],
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      name: 'default',
+      type: 'mongodb',
+      authSource: process.env.NODE_ENV === 'development' ? 'admin' : undefined,
+      database: process.env.MONGODB_DATABASE,
+      url: process.env.MONGODB_CONNECTION_STRING,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([User]),
+  ],
+  controllers: [AppController, UsersController],
   providers: [AppService],
 })
 export class AppModule {}
