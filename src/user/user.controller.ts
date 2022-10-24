8090f0d9c6ab3af15f743d3b5301bdbb6c89edab
Joomonteiro <montjpedro@gmail.com>
2022-10-23 21:38:58 -0300

    feat: EW-4 implements login flow

---
+++ src/user/user.controller.ts
@@ -0,0 +1,16 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { IsPublic } from 'src/auth/decorators/is-public.decorator';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UserService } from './user.service';
+
+@ApiTags('users')
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.userService.create(createUserDto);
+  }
+}
