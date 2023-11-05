e7a3160bed338c3ed6692f99135bbae7bf0169e2
João Pedro Monteiro Pereira <montjpedro@gmail.com>
2023-11-05 17:35:12 -0300

    style(spreadsheet.entity): ED-3 Lint fixes

--- src/spreadsheets/entities/spreadsheet.entity.ts
+++ src/spreadsheets/entities/spreadsheet.entity.ts
@@ -1,12 +1,18 @@
-import { Entity, ObjectID, ObjectIdColumn, Column, CreateDateColumn } from 'typeorm';
+import {
+  Entity,
+  ObjectID,
+  ObjectIdColumn,
+  Column,
+  CreateDateColumn,
+} from 'typeorm';
 
 @Entity('spreadsheet')
 export class Spreadsheet {
   @ObjectIdColumn() id: ObjectID;
   @Column() name: string;
   @Column() user_id: string;
-  @Column() user_name:string;
-  @Column() user_email:string;
+  @Column() user_name: string;
+  @Column() user_email: string;
   @Column() user_color: string;
   @Column() file_s3_key: string;
   @Column() file_original_name: string;
