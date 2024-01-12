e7945e266b0b4aa733a3577aeac84ee8a413a8ed
João Pedro Monteiro Pereira <montjpedro@gmail.com>
2024-01-12 20:36:20 -0300

    feat(user.controller): ED-9 Implements duplicate email verification

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,11 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpException,
+  HttpStatus,
+  Post,
+} from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
-import { IsPublic } from 'src/auth/decorators/is-public.decorator';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserService } from './user.service';
 
@@ -9,9 +14,15 @@ import { UserService } from './user.service';
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @ApiBearerAuth()  
+  @ApiBearerAuth()
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
+  async create(@Body() createUserDto: CreateUserDto) {
+    const emailExists = await this.userService.findByEmail(createUserDto.email);
+
+    if (emailExists) {
+      throw new HttpException('E-mail já está em uso', HttpStatus.BAD_REQUEST);
+    }
+
     return this.userService.create(createUserDto);
   }
 }
