d759a11164383362f702a5856117f29b87b5d89b
João Pedro Monteiro Pereira <montjpedro@gmail.com>
2024-01-12 21:43:32 -0300

    feat(user.controller): ED-11 Adds Role guard to create method and roles to allow only admin user to create another user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpException,
+  HttpStatus,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
-import { IsPublic } from 'src/auth/decorators/is-public.decorator';
+import { Roles } from 'src/auth/decorators/roles.decorators';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { EnumRoles } from 'src/models';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserService } from './user.service';
 
@@ -9,9 +18,17 @@ import { UserService } from './user.service';
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @ApiBearerAuth()  
+  @ApiBearerAuth()
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
+  @Roles(EnumRoles.ADMIN)
+  @UseGuards(RolesGuard)
+  async create(@Body() createUserDto: CreateUserDto) {
+    const emailExists = await this.userService.findByEmail(createUserDto.email);
+
+    if (emailExists) {
+      throw new HttpException('E-mail já está em uso', HttpStatus.BAD_REQUEST);
+    }
+
     return this.userService.create(createUserDto);
   }
 }
