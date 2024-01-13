0c5ba04ecb1e132f194990d768f956860af6b5c7
João Pedro Monteiro Pereira <montjpedro@gmail.com>
2024-01-12 21:40:32 -0300

    feat(UserFromJwt): ED-11 Adds doc | role key

--- src/auth/models/UserFromJwt.ts
+++ src/auth/models/UserFromJwt.ts
@@ -1,5 +1,13 @@
+import { EnumRoles } from 'src/models';
+
+/**
+ * UserFromJwt
+ *
+ * This is the user object that will be returned from the validate() method of the JwtStrategy class.
+ */
 export class UserFromJwt {
   id: string;
   email: string;
   name: string;
+  role: EnumRoles;
 }
