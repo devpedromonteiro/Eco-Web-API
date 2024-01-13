85d70c45e37497d337b89dac8b88e74266470bb8
João Pedro Monteiro Pereira <montjpedro@gmail.com>
2024-01-12 21:40:55 -0300

    feat(UserPayload): ED-11 Adds doc | role key

--- src/auth/models/UserPayload.ts
+++ src/auth/models/UserPayload.ts
@@ -1,7 +1,15 @@
+import { EnumRoles } from 'src/models';
+
+/**
+ * UserPayload
+ *
+ * This is the user object that will be encoded into the JWT.
+ */
 export interface UserPayload {
   sub: string;
   email: string;
   name: string;
+  role: EnumRoles;
   iat?: number;
   exp?: number;
 }
